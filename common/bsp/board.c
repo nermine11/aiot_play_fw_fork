b1d085aa94ff5e41abb10d95ba470b46978e13ff
Thomas Watteyne <twatteyne@gmail.com>
2022-10-23 11:50:41 +0200

    bsp: board

---
+++ common/bsp/board.c
@@ -0,0 +1,26 @@
+#include "board.h"
+
+//=========================== variables =======================================
+
+//=========================== prototypes ======================================
+
+//=========================== public ==========================================
+
+void board_init(void) {
+    
+    // start 32kHz XTAL
+    NRF_CLOCK->LFCLKSRC                = 0x00000001; // 1==XTAL
+    NRF_CLOCK->EVENTS_LFCLKSTARTED     = 0;
+    NRF_CLOCK->TASKS_LFCLKSTART        = 0x00000001;
+    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0);
+    
+    // start HFCLK
+    NRF_CLOCK->EVENTS_HFCLKSTARTED     = 0;
+    NRF_CLOCK->TASKS_HFCLKSTART        = 0x00000001;
+    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
+}
+
+//=========================== private =========================================
+
+//=========================== interrupt handlers ==============================
+
