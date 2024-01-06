83427de8530a97152f183729fe22eca6fc15921f
Thomas Watteyne <twatteyne@gmail.com>
2024-01-06 22:37:15 +0100

    #30. Fix.

---
+++ common/drv/music/music.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include "music.h"
+#include "pwm.h"
+
+//=========================== variables =======================================
+
+typedef struct {
+    uint32_t dummy;
+} music_vars_t;
+
+music_vars_t music_vars;
+
+//=========================== prototypes ======================================
+
+//=========================== public ==========================================
+
+void music_init(void) {
+    
+    // TODO
+}
+
+void music_play(song_t song) {
+    
+    // TODO
+}
+
+//=========================== private =========================================
+
+//=========================== interrupt handlers ==============================
