13821aae001eba8a2fd375c02e8f60c8a141c80d
Zimrahin <diegobadillosj@gmail.com>
2024-03-05 15:36:06 +0100

    LaboMap firmware: i2c_begin and i2c_end functions

--- 02drv_sht31/02drv_sht31.c
+++ 02drv_sht31/02drv_sht31.c
@@ -19,23 +19,9 @@ I2C:
 
 #define MOTOR_SPEED 20 // [0...100]
 uint8_t I2CBUF_MOTOR_LEFT_FWD[]   = {0x01,0x02,MOTOR_SPEED,0};
-uint8_t I2CBUF_MOTOR_LEFT_BACK[]  = {0x01,0x01,MOTOR_SPEED,0};
-uint8_t I2CBUF_MOTOR_LEFT_STOP[]  = {0x01,0x02,          0,0};
-uint8_t I2CBUF_MOTOR_RIGHT_FWD[]  = {0x02,0x02,MOTOR_SPEED,0};
-uint8_t I2CBUF_MOTOR_RIGHT_BACK[] = {0x02,0x01,MOTOR_SPEED,0};
-uint8_t I2CBUF_MOTOR_RIGHT_STOP[] = {0x02,0x02,          0,0};
 
-#define LED_INTENSITY 0xff // [0x00...0xff]
-uint8_t I2CBUF_LED_LEFT_WHITE[]   = {0x04,LED_INTENSITY,LED_INTENSITY,LED_INTENSITY};
-uint8_t I2CBUF_LED_LEFT_RED[]     = {0x04,LED_INTENSITY,         0x00,         0x00};
-uint8_t I2CBUF_LED_LEFT_GREEN[]   = {0x04,         0x00,LED_INTENSITY,         0x00};
-uint8_t I2CBUF_LED_LEFT_BLUE[]    = {0x04,         0x00,         0x00,LED_INTENSITY};
-uint8_t I2CBUF_LED_LEFT_OFF[]     = {0x04,         0x00,         0x00,         0x00};
-uint8_t I2CBUF_LED_RIGHT_WHITE[]  = {0x08,LED_INTENSITY,LED_INTENSITY,LED_INTENSITY};
-uint8_t I2CBUF_LED_RIGHT_RED[]    = {0x08,LED_INTENSITY,         0x00,         0x00};
-uint8_t I2CBUF_LED_RIGHT_GREEN[]  = {0x08,         0x00,LED_INTENSITY,         0x00};
-uint8_t I2CBUF_LED_RIGHT_BLUE[]   = {0x08,         0x00,         0x00,LED_INTENSITY};
-uint8_t I2CBUF_LED_RIGHT_OFF[]    = {0x08,         0x00,         0x00,         0x00};
+#define SHT31_ADDR 0X44 
+
 
 void i2c_init(void) {
    //  3           2            1           0
@@ -79,16 +65,33 @@ void i2c_init(void) {
     // 1098 7654 3210 9876 5432 1098 7654 3210
     // AAAA AAAA AAAA AAAA AAAA AAAA AAAA AAAA A: FREQUENCY: 0x01980000==K100==100 kbps
     NRF_TWI0->FREQUENCY           = 0x01980000;
-
-    //  3           2            1           0
-    // 1098 7654 3210 9876 5432 1098 7654 3210
-    // .... .... .... .... .... .... .AAA AAAA A: ADDRESS: 16
-    // xxxx xxxx xxxx xxxx xxxx xxxx x001 0000 
-    //    0    0    0    0    0    0    1    0 0x00000010
-    NRF_TWI0->ADDRESS             = 0x10;
 }
 
-void i2c_send(uint8_t* buf, uint8_t buflen) {
+void i2c_begin(void) {
+    // Disable TWI
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... .... .... .... AAAA A: ENABLE: 0=Enabled
+    // xxxx xxxx xxxx xxxx xxxx xxxx xxxx 0000 
+    //    0    0    0    0    0    0    0    0 0x00000000
+    NRF_TWI0->ENABLE              = 0x00000000;
+}
+
+void i2c_end(void) {
+    // Enable TWI
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... .... .... .... AAAA A: ENABLE: 5=Enabled
+    // xxxx xxxx xxxx xxxx xxxx xxxx xxxx 0101 
+    //    0    0    0    0    0    0    0    5 0x00000005
+    NRF_TWI0->ENABLE              = 0x00000005;
+}
+
+
+
+void i2c_send(uint8_t addr, uint8_t reg, uint8_t* buf, uint8_t buflen) {
+
+    NRF_TWI0->ADDRESS             = addr;
     uint8_t i;
 
     i=0;
@@ -106,39 +109,38 @@ void i2c_send(uint8_t* buf, uint8_t buflen) {
     NRF_TWI0->TASKS_STOP     = 1;
 }
 
+void db_i2c_read_regs(uint8_t addr, uint8_t reg, void *data, size_t len) {
+    DB_TWIM->ADDRESS       = addr;
+    DB_TWIM->TXD.MAXCNT    = 1;
+    DB_TWIM->TXD.PTR       = (uint32_t)&reg;
+    DB_TWIM->RXD.PTR       = (uint32_t)data;
+    DB_TWIM->RXD.MAXCNT    = (uint8_t)len;
+    DB_TWIM->SHORTS        = (1 << TWIM_SHORTS_LASTTX_STARTRX_Pos) | (1 << TWIM_SHORTS_LASTRX_STOP_Pos);
+    DB_TWIM->TASKS_STARTTX = 1;
+    _wait_for_transfer();
+}
+
+void db_i2c_write_regs(uint8_t addr, uint8_t reg, const void *data, size_t len) {
+    assert(len + 1 <= DB_TWIM_TX_BUF_SIZE);
+    // concatenate register address and input data in a single TX buffer
+    _i2c_tx_vars.buffer[0] = reg;
+    memcpy(&_i2c_tx_vars.buffer[1], data, len);
+
+    // send the content to write to the register
+    DB_TWIM->ADDRESS       = addr;
+    DB_TWIM->TXD.PTR       = (uint32_t)_i2c_tx_vars.buffer;
+    DB_TWIM->TXD.MAXCNT    = (uint8_t)len + 1;
+    DB_TWIM->SHORTS        = (1 << TWIM_SHORTS_LASTTX_STOP_Pos);
+    DB_TWIM->TASKS_STARTTX = 1;
+    _wait_for_transfer();
+}
+
 int main(void) {
     
     i2c_init();
 
-    // motor left
-    i2c_send(I2CBUF_MOTOR_LEFT_FWD,    sizeof(I2CBUF_MOTOR_LEFT_FWD));
-    i2c_send(I2CBUF_MOTOR_LEFT_BACK,   sizeof(I2CBUF_MOTOR_LEFT_BACK));
-    i2c_send(I2CBUF_MOTOR_LEFT_STOP,   sizeof(I2CBUF_MOTOR_LEFT_STOP));
-    // motor right
-/*
-motor control
-[
-    motor,     // 0x01: left,    0x02: right
-    direction, // 0x02: forward, 0x01: backward
-    speed,     // between 0 and 100
-    0,
-]
-*/
-    i2c_send(I2CBUF_MOTOR_RIGHT_FWD,   sizeof(I2CBUF_MOTOR_RIGHT_FWD));
-    i2c_send(I2CBUF_MOTOR_RIGHT_BACK,  sizeof(I2CBUF_MOTOR_RIGHT_BACK));
-    i2c_send(I2CBUF_MOTOR_RIGHT_STOP,  sizeof(I2CBUF_MOTOR_RIGHT_STOP));
-    // led left
-    i2c_send(I2CBUF_LED_LEFT_WHITE,    sizeof(I2CBUF_LED_LEFT_WHITE));
-    i2c_send(I2CBUF_LED_LEFT_RED,      sizeof(I2CBUF_LED_LEFT_RED));
-    i2c_send(I2CBUF_LED_LEFT_GREEN,    sizeof(I2CBUF_LED_LEFT_GREEN));
-    i2c_send(I2CBUF_LED_LEFT_BLUE,     sizeof(I2CBUF_LED_LEFT_BLUE));
-    i2c_send(I2CBUF_LED_LEFT_OFF,      sizeof(I2CBUF_LED_LEFT_OFF));
-    // led right
-    i2c_send(I2CBUF_LED_RIGHT_WHITE,   sizeof(I2CBUF_LED_RIGHT_WHITE));
-    i2c_send(I2CBUF_LED_RIGHT_RED,     sizeof(I2CBUF_LED_RIGHT_RED));
-    i2c_send(I2CBUF_LED_RIGHT_GREEN,   sizeof(I2CBUF_LED_RIGHT_GREEN));
-    i2c_send(I2CBUF_LED_RIGHT_BLUE,    sizeof(I2CBUF_LED_RIGHT_BLUE));
-    i2c_send(I2CBUF_LED_RIGHT_OFF,     sizeof(I2CBUF_LED_RIGHT_OFF));
+    i2c_send(SHT31_ADDR, I2CBUF_MOTOR_LEFT_FWD, sizeof(I2CBUF_MOTOR_LEFT_FWD));
+
 
     while(1);
 }
